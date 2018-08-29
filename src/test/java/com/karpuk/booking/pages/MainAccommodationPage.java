44c8e1d698acd982023d2340a9c18c599eac0eff
kateDF <kate.ff@ya.ru>
2018-08-29 22:26:57 +0300

    Add select currency and language methods

--- src/test/java/com/karpuk/booking/pages/MainAccommodationPage.java
+++ src/test/java/com/karpuk/booking/pages/MainAccommodationPage.java
@@ -6,15 +6,31 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.security.InvalidParameterException;
+import java.util.List;
+
 public class MainAccommodationPage extends AbstractPage {
 
     private static final String BASE_URL = "https://www.booking.com";
+
+    private static final String CURRENCY_XPATH = "//li[@data-id='currency_selector']/a";
+    private static final String ALL_CURRENCIES_XPATH = "//ul[@class='currency_list']/li";
+
+    private static final String LANGUAGE_SELECTOR_XPATH = "//li[@data-id='language_selector']/a";
+    private static final String ALL_LANGUAGES_XPATH = "//ul[@class='language_flags']/li";
+
     private static final String REGION_SEARCH_FIELD_ID = "ss";
     private static final String REGION_OPTION_XPATH = "//ul/li[@role='option']";
     private static final String CHECKIN_DATE_XPATH = "//div[@data-mode='checkin']//button";
     private static final String CHECKOUT_DATE_XPATH = "//div[@data-mode='checkout']//button";
     private static final String GUESTS_LABEL_XPATH = "//label[@id='xp__guests__toggle']";
     private static final String SUBMIT_SEARCH_FORM_XPATH = "//div[@class='xp__button']//button";
+
+    @FindBy(xpath = CURRENCY_XPATH)
+    private WebElement currencyButton;
+
+    @FindBy(xpath = LANGUAGE_SELECTOR_XPATH)
+    private WebElement languageButton;
 
     @FindBy(id = REGION_SEARCH_FIELD_ID)
     private WebElement regionSearchField;
@@ -30,6 +46,9 @@ public class MainAccommodationPage extends AbstractPage {
 
     @FindBy(xpath = SUBMIT_SEARCH_FORM_XPATH)
     private WebElement searchButton;
+
+    private List<WebElement> allCurrencies;
+    private List<WebElement> allLanguages;
 
     public MainAccommodationPage(WebDriver driver) {
         super(driver);
@@ -61,4 +80,41 @@ public class MainAccommodationPage extends AbstractPage {
     public void clickSearchButton() {
         searchButton.click();
     }
+
+    public boolean selectCurrency(String currencyName) {
+        String actualCurrency = currencyButton.getText();
+        if (!actualCurrency.equals(currencyName.toUpperCase())) {
+            currencyButton.click();
+            return setCurrency(currencyName);
+            }
+        return true;
+    }
+
+    public boolean selectLanguage(String language) {
+        languageButton.click();
+        return setLanguage(language);
+    }
+
+    private boolean setCurrency(String currencyName) {
+        allCurrencies = driver.findElements(By.xpath(ALL_CURRENCIES_XPATH));
+        for (WebElement cur : allCurrencies) {
+            if (currencyName.equalsIgnoreCase(cur.getAttribute("data-lang"))) {
+                cur.findElement(By.tagName("a")).click();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean setLanguage(String language) {
+        allLanguages = driver.findElements(By.xpath(ALL_LANGUAGES_XPATH));
+        for (WebElement lang : allLanguages) {
+            if (lang.getText().equalsIgnoreCase(language)) {
+                lang.findElement(By.tagName("a")).click();
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/test/java/com/karpuk/booking/steps/CommonSteps.java
+++ src/test/java/com/karpuk/booking/steps/CommonSteps.java
@@ -1,6 +1,7 @@
 package com.karpuk.booking.steps;
 
 import com.karpuk.booking.driver.DriverSingleton;
+import com.karpuk.booking.pages.MainAccommodationPage;
 import org.openqa.selenium.WebDriver;
 
 public class CommonSteps {
@@ -16,6 +17,18 @@ public class CommonSteps {
 
     public void closeDriver() {
         DriverSingleton.closeDriver();
+    }
+
+    public boolean setCurrency(String currencyName) {
+        MainAccommodationPage mainPage = new MainAccommodationPage(driver);
+        mainPage.openPage();
+        return mainPage.selectCurrency(currencyName);
+    }
+
+    public boolean setLanguage(String language) {
+        MainAccommodationPage mainPage = new MainAccommodationPage(driver);
+        mainPage.openPage();
+        return mainPage.selectLanguage(language);
     }
 
 }
